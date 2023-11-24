c0889427c9535e20182b0a661e2d41fc97f7c7f2
kloe-b <burazerikloena@gmail.com>
2023-11-24 09:31:21 +0700

    restructure code

--- payment-service/src/app.py
+++ payment-service/src/app.py
@@ -1,34 +1,82 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from redis import Redis
+import json
+from database import db, Payment, User, bcrypt
 import os
-from flask_cors import CORS
-from payment.payment_service import payment_service
-from database import db, bcrypt
-import logging
+import threading
 
-def create_app() -> Flask:
-    """Create flask app."""
-    app = Flask(__name__)
-    
-    CORS(app)
-    app.secret_key = os.environ.get("SECRET_KEY", 'your_secret_key')
-    app.register_blueprint(payment_service)
-    ORDER_SERVICE_URL = "http://127.0.0.1:8080" 
+app = Flask(__name__)
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] =\
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
   
-    db.init_app(app)
-    bcrypt.init_app(app)
+db.init_app(app)
+bcrypt.init_app(app)
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 
+@app.route('/process_payment', methods=['POST'])
+def process_payment():
+    data = request.json
+    user_id = data['user_id']
+    order_id = data['order_id']
+    purchase_amount = data['amount']
+
+    user = User.get_or_create(user_id)
+
+    if user.credits < purchase_amount:
+        return jsonify({'status': 'INSUFFICIENT_FUND', 'message': 'Not enough credits'}), 400
+
+    try:
+        # time.sleep(5)  
+        user.credits -= purchase_amount
+        db.session.commit()
+        r.publish('payment_status', json.dumps({'order_id': order_id, 'status': 'SUCCESS'}))
+        return jsonify({'status': 'SUCCESS', 'message': 'Payment processed', 'remaining_credits': user.credits})
+    
+    except TimeoutError:
+        return jsonify({'status': 'TIMEOUT', 'message': 'Payment processing timeout'}), 500
+    except:
+        db.session.rollback()
+        r.publish('payment_status', json.dumps({'order_id': order_id, 'status': 'UNKNOWN'}))
+        return jsonify({'status': 'UNKNOWN', 'message': 'An error occurred'}), 500
+
+
+def handle_order_created_event():
+    pubsub = r.pubsub()
+    pubsub.subscribe('order_created')
+
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            order_data = json.loads(message['data'])
+            print(f"Order created event received: {order_data}")
+
+def rollback_payment(order_id):
+    payment = Payment.query.filter_by(order_id=order_id, status='SUCCESS').first()
+    if payment:
+        user = User.query.get(payment.user_id)
+        user.credits += payment.amount
+        payment.status = 'REFUNDED'
+        db.session.commit()
+        r.publish('payment_rollback', json.dumps({'order_id': order_id, 'status': 'REFUNDED'}))
+
+def handle_inventory_failure_event(message):
+    data = json.loads(message['data'])
+    order_id = data['order_id']
+    rollback_payment(order_id)
+
+r.pubsub().subscribe(**{'inventory_failure': handle_inventory_failure_event})
+
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    return app
-
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8081))
-    app.run(port=port, debug=True, host='0.0.0.0')
+    thread = threading.Thread(target=handle_order_created_event)
+    thread.start()
+    
+    app.run(debug=True, port=5001)
--- payment-service/src/database.py
+++ payment-service/src/database.py
@@ -5,6 +5,29 @@ import datetime
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
+# Payment model
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(50), nullable=False, default='pending')
+    amount = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'order_id': self.order_id,
+            'status': self.status,
+            'amount': self.amount
+        }
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    credits = db.Column(db.Float, nullable=False, default=100.0)
+    id = db.Column(db.Integer, primary_key=True)
+    credits = db.Column(db.Float, default=100.0)  # Default initial credits
+
+    @staticmethod
+    def get_or_create(user_id):
+        user = User.query.get(user_id)
+        if not user:
+            user = User(id=user_id)
+            db.session.add(user)
+            db.session.commit()
+        return user
